85f499e55913a368e64605a99b745c89513c96f6
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-11-29 01:33:54 +0200

    add icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+
 import s from "../Footer/Footer.module.css";
 
+import logo from "../../images/Express.png";
+import dmca from "../../images/DMCA.png";
+
 function Footer() {
-  return <h1 className={s.footer}>Footer</h1>;
+  return (
+    <>
+      <h1 className={s.footer}>Footer</h1>
+      <img src={dmca} alt="Logo" className={s.dmca} />
+
+      <img src={logo} alt="Logo" className={s.logo} />
+    </>
+  );
 }
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { Navbar, Nav, Button } from "react-bootstrap";
-// import "../Header/Header.module.css";
-import s from "../Header/Header.module.css";
 
+import s from "../Header/Header.module.css";
+import logo from "../../images/Express.png";
 function Header() {
   return (
     <>
       <Navbar collapseOnSelect expand="lg">
-        <Navbar.Brand className={s.logo}>Express</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            alt="Logo"
+            width="110"
+            height="32"
+            className={s.logo}
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar" />
         <Navbar.Collapse id="responsive-navbar">
-          <Nav className={s.navbar}>
+          <Nav className={s.nav}>
             <Nav.Link>
               <Link to="/">Home</Link>
             </Nav.Link>
@@ -25,10 +33,10 @@ function Header() {
             <Nav.Link>
               <Link to="/contact">Contact Us</Link>
             </Nav.Link>
-            <Button className={s.btn} variant="light">
-              Call Us
-            </Button>
           </Nav>
+          <Button className={s.btn} variant="light">
+            Call Us
+          </Button>
         </Navbar.Collapse>
       </Navbar>
     </>
